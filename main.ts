51737b1ef467c2bea8883d53052ebddaf00847fa
Bernd <b.birkicht@thg-hn.de>
2024-09-25 11:49:20 +0200

    mit eventbehandlung pin p0 gepulst hocht/niedrig

--- main.ts
+++ main.ts
@@ -1,13 +1,11 @@
 pins.onPulsed(DigitalPin.P0, PulseValue.High, function () {
-	
+    basic.setLedColor(0xff0000)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+})
+pins.onPulsed(DigitalPin.P0, PulseValue.Low, function () {
+    basic.setLedColor(0x00ff00)
+    pins.digitalWritePin(DigitalPin.P1, 0)
 })
 basic.forever(function () {
-    if (pins.analogReadPin(AnalogPin.P0) > 0) {
-        basic.setLedColor(0xff0000)
-        pins.digitalWritePin(DigitalPin.P1, 1)
-    }
-    if (pins.analogReadPin(AnalogPin.P0) == 0) {
-        basic.setLedColor(0x00ff00)
-        pins.digitalWritePin(DigitalPin.P1, 0)
-    }
+	
 })
