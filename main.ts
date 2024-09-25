c62dc846c963a32c252948755934bf84237576a0
Bernd <b.birkicht@thg-hn.de>
2024-09-25 11:52:16 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 pins.onPulsed(DigitalPin.P0, PulseValue.High, function () {
-    basic.setLedColor(0xff0000)
-    pins.digitalWritePin(DigitalPin.P1, 1)
-})
-pins.onPulsed(DigitalPin.P0, PulseValue.Low, function () {
     basic.setLedColor(0x00ff00)
     pins.digitalWritePin(DigitalPin.P1, 0)
+})
+pins.onPulsed(DigitalPin.P0, PulseValue.Low, function () {
+    basic.setLedColor(0xff0000)
+    pins.digitalWritePin(DigitalPin.P1, 1)
 })
 basic.forever(function () {
 	
